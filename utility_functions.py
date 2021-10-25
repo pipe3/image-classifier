e2783472e704ed21533569ccb12b5c4eb085ad52
Oliver Kuhles <oliver@kuhles.net>
2021-10-25 17:37:47 +0200

    Fixing issue with model.to(device)

--- utility_functions.py
+++ utility_functions.py
@@ -111,8 +111,8 @@ def predict(image_path, model, topk, device):
     image = torch.unsqueeze(image,0)
 
     # Move image and model to the correct device
-    image.to(device)
-    model.to(device)
+    #image.to(device)
+    #model.to(device)
 
     with torch.no_grad():
         model.eval()
