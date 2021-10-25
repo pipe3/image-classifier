320255b5484b29b1b998e3628fb111f2195a2c5e
Oliver Kuhles <oliver@kuhles.net>
2021-10-25 16:19:13 +0200

    First running version of predict

---
+++ predict.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# PROGRAMMER: oliver@kuhles.net
+# DATE CREATED: 25/Oct/2021
+# REVISED DATE:             <=(Date Revised - if any)
+# PURPOSE: Image classifier prediction
+
+# Imports
+import matplotlib.pyplot as plt
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+import json
+from collections import OrderedDict
+import PIL
+from PIL import Image
+import numpy as np
+import time
+from datetime import datetime
+import argparse
+import model_functions, utility_functions
+import sys
+
+def main():
+
+    # argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('imagepath', help='path to single image')
+    parser.add_argument('checkpoint', help='path to model checkpoint')
+    parser.add_argument('--top_k', help='Number of top k most likely classes to predict', type=int, default=5)
+    parser.add_argument('--category_names', help='path to file with category names')
+    parser.add_argument('--gpu', help='Use GPU instead of CPU',  action="store_true", default=False)
+
+    args = parser.parse_args()
+    #print(args)
+
+    image = args.imagepath
+    checkpoint = args.checkpoint
+    topk = args.top_k
+    catfile = args.category_names
+    gpu = args.gpu
+
+    print('Image Classifier prediction starting up')
+
+    # Doublecheck if we have a GPU if the user has selected to use GPU
+    if gpu and torch.cuda.is_available():
+        device = torch.device('cuda')
+        print('Model running on', device)
+    elif gpu and not torch.cuda.is_available():
+        device = torch.device('cpu')
+        print('GPU selected but no GPU identified. Model running on', device)
+    else:
+        device = torch.device('cpu')
+        print('Model running on', device)
+
+    # Load checkpoint
+    model, optimizer, lr_scheduler, epochs = utility_functions.load_checkpoint(checkpoint, device)
+    print(model.classifier)
+
+    #utility_functions.imshow(utility_functions.process_image(image))
+
+    print('Predicting class')
+    probs, labels = utility_functions.predict(image, model, topk, device)
+
+    p_list = probs.squeeze().tolist()
+    l_list = labels.squeeze().tolist()
+
+    print(model.class_to_idx)
+    class_list = utility_functions.get_category_names(catfile, model.class_to_idx, l_list, p_list)
+
+    plt.figure()
+    plt.barh(class_list, p_list)
+    plt.title('Probability Prediction')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
--- utility_functions.py
+++ utility_functions.py
@@ -1,1 +1,146 @@
-# loading and preprocessing the image
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# PROGRAMMER: oliver@kuhles.net
+# DATE CREATED: 25/Oct/2021
+# REVISED DATE:             <=(Date Revised - if any)
+# PURPOSE: Loading and preprocessing the image
+#
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+import json
+from collections import OrderedDict
+import PIL
+from PIL import Image
+import numpy as np
+import time
+from datetime import datetime
+import sys
+
+def load_checkpoint(file, device):
+    '''
+    Loads a model from a saved checkpoint. Moves it to the device (CPU or GPU).
+
+    Returns the model along with other model parameters.
+    '''
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Loading checkpoint into', device)
+    if torch.cuda.is_available():
+        checkpoint = torch.load(file, map_location=lambda storage, loc: storage.cuda(0))
+    else:
+        checkpoint = torch.load(file, map_location=lambda storage, loc: storage)
+
+    #model = models.checkpoint['arch']
+    model = getattr(models, checkpoint['arch'])(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    model.classifier = checkpoint['classifier']
+    model.load_state_dict(checkpoint['state_dict'], strict=False)
+    model.class_to_idx = checkpoint['class_to_idx']
+    optimizer = checkpoint['optimizer']
+    lr_scheduler = checkpoint['lr_scheduler']
+    epochs = checkpoint['epochs']
+
+    return model, optimizer, lr_scheduler, epochs
+
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    img = Image.open(image)
+    #imshow(img)
+    # using thumbnail because it respects ratio
+    img.thumbnail((255,255))
+
+    # center crop
+    crop_size = 224
+    left  = int(img.size[0]/2-crop_size/2)
+    upper = int(img.size[1]/2-crop_size/2)
+    right = left+crop_size
+    lower = upper+crop_size
+
+    cropped_img = img.crop((left,upper,right,lower))
+
+    # use std and mean for ImageNet, same as for normalization in transforms
+    std = [0.229, 0.224, 0.225]
+    mean = [0.485, 0.456, 0.406]
+
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize(mean, std)])
+
+    norm_img = transform(cropped_img)
+
+    return norm_img
+
+
+def imshow(image, ax=None, title=None):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.numpy().transpose((1, 2, 0))
+
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+
+    ax.imshow(image)
+
+    return ax
+
+def get_category_names(filename, class_to_idx, l_list, p_list):
+    '''
+    Returns a list with category names.
+    '''
+    # cat_to_name
+    with open(filename, 'r') as f:
+        cat_to_name = json.load(f)
+
+    i = 0
+    class_list = []
+    for l in l_list:
+        print('L:', l)
+        classification = list(class_to_idx)[l]
+        name = cat_to_name.get(str(classification))
+        #prob = p_list[i]
+        class_list.append(name)
+        i+=1
+
+    return class_list
+
+
+def predict(image_path, model, topk, device):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+
+    # Pre-process the image
+    image = process_image(image_path)
+
+    # Add a dimension for the batch
+    image = torch.unsqueeze(image,0)
+
+    # Move image and model to the correct device
+    image.to(device)
+    model.to(device)
+
+    with torch.no_grad():
+        model.eval()
+        ps = torch.exp(model.forward(image))
+        top_ps, top_class = ps.topk(topk, dim=1)
+        model.train()
+
+    return(top_ps, top_class)
